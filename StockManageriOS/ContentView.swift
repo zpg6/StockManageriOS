618706b1a53fa731ad290852b9913305f22afbd0
Zach <zpg6@pitt.edu>
2020-08-21 17:04:34 -0400

    Initial Commit

---
+++ StockManageriOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StockManageriOS
+//
+//  Created by Zachary Grimaldi on 8/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
