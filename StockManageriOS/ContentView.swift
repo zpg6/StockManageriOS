a4e83f87d7638bbb86359ad1c381233be6e4ea63
Zach <zpg6@pitt.edu>
2020-08-22 12:58:22 -0400

    Initialized Number Pad main display

--- StockManageriOS/ContentView.swift
+++ StockManageriOS/ContentView.swift
@@ -8,9 +8,115 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let spacing: CGFloat = 15
+    @State var typed = ""
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack (spacing: self.spacing) {
+            
+            Text(" \(self.typed) ")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.primary)
+            
+            HStack (spacing: self.spacing) {
+                
+                NumberPadButtonView(buttonText: "1") {
+                    let button = "1"
+                    print(button)
+                    self.typed += button
+                }
+                NumberPadButtonView(buttonText: "2") {
+                    let button = "2"
+                    print(button)
+                    self.typed += button
+                }
+                NumberPadButtonView(buttonText: "3") {
+                    let button = "3"
+                    print(button)
+                    self.typed += button
+                }
+                
+            }
+            
+            HStack (spacing: self.spacing) {
+                
+                NumberPadButtonView(buttonText: "4") {
+                    let button = "4"
+                    print(button)
+                    self.typed += button
+                }
+                NumberPadButtonView(buttonText: "5") {
+                    let button = "5"
+                    print(button)
+                    self.typed += button
+                }
+                NumberPadButtonView(buttonText: "6") {
+                    let button = "6"
+                    print(button)
+                    self.typed += button
+                }
+                
+            }
+            
+            
+            HStack (spacing: self.spacing) {
+                
+                NumberPadButtonView(buttonText: "7") {
+                    let button = "7"
+                    print(button)
+                    self.typed += button
+                }
+                NumberPadButtonView(buttonText: "8") {
+                    let button = "8"
+                    print(button)
+                    self.typed += button
+                }
+                NumberPadButtonView(buttonText: "9") {
+                    let button = "9"
+                    print(button)
+                    self.typed += button
+                }
+                
+            }
+            
+            HStack (spacing: self.spacing) {
+                
+                if self.typed.count > 0 {
+                    NumberPadButtonView(buttonIcon: "xmark") {
+                        let button = "xmark"
+                        print(button)
+                        self.typed = String(self.typed.dropLast())
+                    }
+                } else {
+                    Circle()
+                        .fill(Color.clear)
+                        .frame(width: 105, height: 105)
+                }
+                
+                NumberPadButtonView(buttonText: "0") {
+                    let button = "0"
+                    print(button)
+                    self.typed += button
+                }
+                
+                if self.typed.count > 0 {
+                    NumberPadButtonView(buttonIcon: "arrow.right") {
+                        print("arrow.right")
+                    }
+                } else {
+                    Circle()
+                        .fill(Color.clear)
+                        .frame(width: 105, height: 105)
+                }
+            }
+            
+        }
+        
+        
     }
 }
 
---
+++ StockManageriOS/NumberPadButtonView.swift
@@ -0,0 +1,89 @@
+//
+//  NumberPadButtonView.swift
+//  StockManageriOS
+//
+//  Created by Zachary Grimaldi on 8/21/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct NumberPadButtonView: View {
+    
+    let buttonText: String?
+    let buttonIcon: String?
+    let action: ButtonAction
+    
+    @State private var timer: Timer?
+    @State private var isLongPressing: Bool = false
+    
+    typealias ButtonAction = ()->()
+    
+    init(buttonText: String? = nil, buttonIcon: String? = nil, action: @escaping ButtonAction) {
+        self.buttonText = buttonText
+        self.buttonIcon = buttonIcon
+        self.action = action
+    }
+    
+    func wrappedAction() {
+        print("spot 1")
+        if self.isLongPressing {
+            print("spot 2")
+            self.isLongPressing.toggle()
+            self.timer?.invalidate()
+        }
+        else {
+            print("spot 3")
+            self.action()
+        }
+    }
+    
+    var body: some View {
+        
+        Button(action: self.wrappedAction) {
+            ZStack {
+                
+                Circle()
+                    .fill(Color.primary)
+                    .frame(width: 105, height: 105)
+                
+                Circle()
+                    .fill(Color(.systemGray5))
+                    .frame(width: 100, height: 100)
+                    
+                if self.buttonText != nil {
+                    Text(self.buttonText ?? "")
+                        .font(.largeTitle)
+                        .foregroundColor(.primary)
+                }
+                else if self.buttonIcon != nil {
+                    Image(systemName: buttonIcon ?? "")
+                        .font(.largeTitle)
+                        .foregroundColor(.primary)
+                }
+            }
+        }.simultaneousGesture(LongPressGesture(minimumDuration: 0.5).onEnded({ (_) in
+            self.isLongPressing = true
+            print("spot 4")
+            self.timer = Timer.scheduledTimer(withTimeInterval: 0.25, repeats: true, block: { (_) in
+                self.action()
+                print("spot 5")
+            })
+        }))
+        
+    }
+    
+}
+
+struct NumberPadButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack {
+            NumberPadButtonView(buttonText: "1") {
+                print("1")
+            }
+            NumberPadButtonView(buttonIcon: "xmark") {
+                print("xmark")
+            }
+        }
+    }
+}
